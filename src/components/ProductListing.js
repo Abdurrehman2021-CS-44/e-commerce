83ab9b30dd1a7ae02bf5bbe431f13bdcf642188f
Abdurrehman_Khan <abdurrehmanbinfaheem@gmail.com>
2023-08-07 15:45:11 +0500

    Implement search functionality

--- src/components/ProductListing.js
+++ src/components/ProductListing.js
@@ -1,22 +1,43 @@
 import Product from "./Product";
 import products from "../products";
+import { useState } from "react";
 
 const ProductListing = () => {
+
+    const [search, setSearch] = useState("");
+    const [availableProducts, setAvailableProducts] = useState(products);
+
+    const handleChange = (e) => {
+        const inputValue = e.target.value;
+        setSearch(inputValue);
+    }
+
+    const handleClick = () => {
+        const searchedItems = products.filter((product) => product.title.includes(search));
+        setAvailableProducts(searchedItems);
+    }
+
     return (
         <>
             <h1 className="title">Abdur Rehman's Shop</h1>
             <div className="container">
-                <input
-                    type="text"
-                    name="search"
-                    className="form-control search-bar"
-                    placeholder="🔎 Search products"
-                />
+                <div class="input-group input-group-lg">
+                    <input
+                        type="text"
+                        class="form-control search-bar"
+                        aria-label="Large"
+                        aria-describedby="inputGroup-sizing-sm"
+                        placeholder="Search products"
+                        value={search}
+                        onChange={handleChange}
+                    />
+                    <button className="btn btn-primary px-4" onClick={handleClick}> <i class="bi bi-search"></i> </button>
+                </div>
             </div>
             <div className="container my-5">
                 <div className="row">
                     {
-                        products.map((product, index) => {
+                        availableProducts.map((product, index) => {
                             return (
                                 <div className="col-lg-4 col-md-6 my-4" key={index}>
                                     <Product
